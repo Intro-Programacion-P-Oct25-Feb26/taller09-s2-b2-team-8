1aa79e011f99c378da45ba7d09804359a1417e9c
jhon pardo <jhonedupardo15@gmail.com>
2025-12-11 12:39:47 -0500

    Solucion ejercicio04

--- EjerciciosT9/src/paquete02/Ejercicio04.java
+++ EjerciciosT9/src/paquete02/Ejercicio04.java
@@ -17,11 +17,39 @@ public class Ejercicio04 {
     public static void main(String[] args) {
         
         double[] promedios = {10, 10, 9.1, 7, 6.1, 4, 8};
-        String[] estudiantes = {"Kimberly Gonzalez", "Mark Hogan", 
+        String[] estudiantes = {"Kimberly Gonzalez", "Mark Hogan",
             "Teresa Martinez", "Julia Johnson", "Mark Cook", "Jennifer Manning",
             "Juan Vasquez"};
-        String[] promediosCualitativos = new String[7];
-        
-    }
 
+        String[] promediosCualitativos = new String[7];
+        double nota;
+        String cualitativo;
+
+        int valor= promedios.length;
+        for (int i = 0; i < valor; i++) {
+
+            nota = promedios[i];
+
+            if (nota >= 0 && nota <= 5.9) {
+                cualitativo = "Regular";
+            } else {
+                if (nota >= 6 && nota <= 8.9) {
+                    cualitativo = "Bueno";
+                } else {
+                    cualitativo = "Sobresaliente"; 
+                }
+            }
+
+            promediosCualitativos[i] = cualitativo;
+        }
+
+        System.out.println("Reporte de promedios de estudiantes:\n");
+        int valor2= estudiantes.length;
+        for (int i = 0; i < valor2; i++) {
+            System.out.printf("%s promedio: %.2f promedio cualitativo: %s\n",
+                    estudiantes[i],
+                    promedios[i],
+                    promediosCualitativos[i]);
+        }
+    }
 }
