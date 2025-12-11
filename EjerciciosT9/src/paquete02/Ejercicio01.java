044a58edf639939b80840186f820372ac56f1f8b
poleagui06 <jeanpoleagui06@gmail.com>
2025-12-11 12:01:46 -0500

    Solucion ejercicio01

--- EjerciciosT9/src/paquete02/Ejercicio01.java
+++ EjerciciosT9/src/paquete02/Ejercicio01.java
@@ -23,23 +23,23 @@ public class Ejercicio01 {
         String[] rangos = {"1-5", "6-10", "11-15", "16-20"};
         int[] valoresRangos = new int[4]; // {0, 0, 0, 0}
         int respuesta;
-        
+                            
         for (int i = 0; i < respuestas.length; i++) { 
             respuesta = respuestas[i]; 
                     
-            if ((respuesta >= 1) || (respuesta <=5)){
-                valoresRangos[0] = valoresRangos[0] + 1;
+            if ((respuesta >= 1) && (respuesta <=5)){
+                valoresRangos[0] = valoresRangos[0] + 10;
                 
             }else{
                     
-                if ((respuesta >= 6) || (respuesta <=10)){
-                    valoresRangos[1] = valoresRangos[1] + 1;
+                if ((respuesta >= 6) && (respuesta <=10)){
+                    valoresRangos[1] = valoresRangos[1] + 5;
                 }else{
                             
-                    if ((respuesta >= 11) || (respuesta <=15)){
-                        valoresRangos[2] = valoresRangos[2] + 1; //3+1 
+                    if ((respuesta >= 11) && (respuesta <=15)){
+                        valoresRangos[2] = valoresRangos[2] + 0; //3+1 
                     }else{
-                        if ((respuesta >= 16) || (respuesta <=20)){
+                        if ((respuesta >= 16) && (respuesta <=20)){
                             valoresRangos[3] = valoresRangos[3] + 1;//1+1
                         }
                     
