bce00c7aba900991107f42b0f1c49957cf72f57f
poleagui06 <jeanpoleagui06@gmail.com>
2025-12-11 17:33:03 -0500

    Solucion ejercicio07

--- EjerciciosT9/src/paquete02/Ejercicio07.java
+++ EjerciciosT9/src/paquete02/Ejercicio07.java
@@ -17,8 +17,29 @@ public class Ejercicio07 {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-      
+        Scanner entrada = new Scanner(System.in);
+        
+        int[] numeros = new int[10];
+        int suma = 0;
+        double promedio;
+        System.out.println("Ingrese 10 números:");
+        
+        for (int i = 0; i < numeros.length; i++) {
+            
+            System.out.println("Número " + (i + 1) + ":");
+            numeros[i] = entrada.nextInt();
+            suma += numeros[i];
+            
+        }
+        promedio = (double) suma / numeros.length;
+        System.out.println("\nResultados:");
+        for (int i = 0; i < numeros.length; i++) {
+            double diferencia = numeros[i] - promedio;
+            System.out.printf("Número: %d | Diferencia con el promedio: %.2f%n", numeros[i], diferencia);
+        }
 
+
+        
     }
 
 }
